02470eee45146730e69590375be78da16a328625
Vera <solnce288888@gmail.com>
2024-03-04 22:48:20 +0300

    hw5_admin_edited

--- store_app/admin.py
+++ store_app/admin.py
@@ -5,14 +5,59 @@ from .models import *
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'name', 'reg_date']
+    list_filter = ['name', 'reg_date']
+    list_sort = ['id', 'name', 'reg_date']
+    fieldsets = [('Общие данные:',
+                  {'classes': ['wide'],
+                   'fields': ['name', 'phone', 'reg_date'],
+                   },
+                  ),
+                 ('Подробности:',
+                  {'classes': ['collapse'],
+                   'description': 'Полная информация',
+                   'fields': ['email', 'address'],
+                   },
+                  ),
+                 ]
+    readonly_fields = ['id', 'reg_date']
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'name', 'price']
+    list_filter = ['price', 'name']
+    list_sort = ['id', 'name', 'price']
+    fieldsets = [('Общие данные:',
+                    {'classes': ['wide'],
+                     'fields': ['name', 'price'],
+                    },
+                ),
+                ('Подробности:',
+                    {'classes': ['collapse'],
+                     'description': 'Полная информация',
+                     'fields': ['description', 'quantity', 'image'],
+                    },
+                ),
+            ]
+    readonly_fields = ['id']
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'customer', 'total_price']
+    list_filter = ['customer', 'products']
+    list_sort = ['id', 'customer', 'total_price', 'order_date']
+    fieldsets = [('Общие данные:',
+                    {'classes': ['wide'],
+                     'fields': ['customer', 'total_price'],
+                    },
+                ),
+                ('Подробности:',
+                    {'classes': ['collapse'],
+                     'description': 'Полная информация',
+                     'fields': ['products', 'order_date'],
+                    },
+                ),
+            ]
+    readonly_fields = ['id', 'order_date']
